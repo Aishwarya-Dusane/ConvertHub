aaf3f52cc90318c3be7a751ec5803b32d96aab9b
Viraj Ghorpade <viraj.ghorpade9767@gmail.com>
2025-08-26 13:01:37 +0530

    fix: issue which occurs while creating build 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Provider } from "react-redux";
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
